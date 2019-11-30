1e9b522bcc079f2d5bd217b0899e7d5884c98b73
dragon081223 <mantine08@gmail.com>
2019-11-30 13:40:35 -0800

    created login interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LoginComponent from "./components/LoginComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Lets yeet this hackathon
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React you plebs
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <LoginComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
