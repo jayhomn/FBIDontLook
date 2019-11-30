0f474b6056e6213db49d980381dba05c7110c301
dragon081223 <mantine08@gmail.com>
2019-11-29 18:28:58 -0800

    added rest framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'piazzacloud',
+	'corsheaders',
+	'rest_framework',
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
