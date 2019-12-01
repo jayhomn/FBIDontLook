4f2eaa9c1bd5924c39a157039c000caadb0ff7f5
ivhuak <ivan.hua12@gmail.com>
2019-11-30 19:11:25 -0800

    top level assembly

--- backend/wordCloudBackend/piazzaParser.py
+++ backend/wordCloudBackend/piazzaParser.py
@@ -1,8 +1,11 @@
 from operator import itemgetter
+from backend.piazzaAPI import *
+from backend.backend.freqdist import *
 from collections import OrderedDict
 
 
-def sort(dictionary, cutoff):
+def sort(email, password, coursecode, cutoff=30):
+    dictionary = raw_dict_maker(piazza_reader(email, password, coursecode))
     if len(dictionary) <= cutoff:
         sort_dictionary = sorted(dictionary.items(), key=itemgetter(1))
         return list(reversed(sort_dictionary))
@@ -11,3 +14,4 @@ def sort(dictionary, cutoff):
         new_dictionary = list(reversed(sort_dictionary))[:cutoff]
         return new_dictionary
 
+print(sort('kevinpkf311@gmail.com', 'pkf88063302', 'MATH 253.ALL'))
