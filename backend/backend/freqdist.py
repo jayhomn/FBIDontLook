5f475da17ae798f09ae1293fb61eb7733ee6d7f9
KevinetPeng <kevinpkf311@gmail.com>
2019-12-06 17:15:34 -0800

    implemented master list for known course codes, and implemented final piazza dictionary methods

--- backend/backend/freqdist.py
+++ backend/backend/freqdist.py
@@ -1,4 +1,4 @@
-from backend.piazzaAPI import *  # looks for file location and imports everything with the star
+from backend.wordCloudBackend.restAPI.piazzaAPI import *  # looks for file location and imports everything with the star
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 import string
@@ -12,23 +12,34 @@ custom_stopwords = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", 
                     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
-def raw_dict_maker(stringin):
+def piazzaDictionaryGenerator(email, password, coursecode):
+
+    stringin = piazza_reader(email, password, coursecode)
+
     # define stop/filler words and punctuation
     stop = stopwords.words('english') + list(string.punctuation) + custom_stopwords
     cleaner = TweetTokenizer()
 
     # string stripped of punctuation and filler words
-    cleanstring = [i for i in cleaner.tokenize(stringin.lower()) if i not in stop]
+    cleanString = [i for i in cleaner.tokenize(stringin.lower()) if (i not in stop)]
+
+    # iterate through the string array, and get rid of non ascii characters; reduced to ''
+    printable = set(string.printable)
+    for index in range(len(cleanString)):
+        cleanString[index] = ''.join(filter(lambda x: x in printable, cleanString[index]))
 
     # frequency distribution of cleaned string
-    fdist = FreqDist(cleanstring)
+    fdist = FreqDist(cleanString)
 
-    # top 50 distribution
-    fdist_50 = fdist.most_common(50)
+    # get rid of empty key due to non-ascii removal from previous step
+    del fdist['']
 
-    # convert to dictionary
+    # top 30 distribution
+    fdist_30 = fdist.most_common(30)
+
+    # convert to dictionary and return dictionary
     freq_dictionary = {}
-    for index in range(len(fdist_50)):
-        freq_dictionary.update({fdist_50[index][0]: fdist_50[index][1]})
+    for index in range(len(fdist_30)):
+        freq_dictionary.update({fdist_30[index][0]: fdist_30[index][1]})
 
     return freq_dictionary
